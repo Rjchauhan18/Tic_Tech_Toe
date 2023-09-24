912f82d8e123f4c2d00659987b096ba06ab3e8ff
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-09-24 15:26:08 +0530

    done

--- BharatKrishiSarthi/settings.py
+++ BharatKrishiSarthi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k&#7l6!(wv=rib)xec+s^f7zj1gk@zlgp+$_0^w83@8*_@-q*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['127.0.0.1','bharatkrishisarthi.onrender.com']
